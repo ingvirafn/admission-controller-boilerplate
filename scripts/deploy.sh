8363b73495edc6442448c1fcb2ba5a19d4944d0b
Ingvi Rafn <ingvirafn@ingvirafn.com>
2020-04-29 21:14:17 +0000

    admission-controller v1 - untested

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+set -euo pipefail
+
+basedir="$(dirname "$0")"
+keydir="$(dirname "$0")/../keys"
+
+# Generate keys
+if test -f "$keydir/tls.crt"; then
+    echo "Certificate exists, not creating"
+else
+    echo "Generating TLS keys ..."
+    "${basedir}/generate-keys.sh" "$keydir"
+fi
+
+ca_pem_b64="$(openssl base64 -A <"${keydir}/ca.crt")"
+tls_crt_b64="$(openssl base64 -A <"${keydir}/tls.crt")"
+tls_key_b64="$(openssl base64 -A <"${keydir}/tls.key")"
+
+cmd="template"
+
+# if [ $1 = "install" ]; then
+#     cmd="install"
+# fi
+
+helm $cmd chart/admission-controller/ --set ca_pem_b64=$ca_pem_b64 --set tls.crt=$tls_crt_b64 --set tls.key=$tls_key_b64
+
+
---
+++ src/admission-controller/index.js
@@ -0,0 +1,29 @@
+const https = require("https"),
+  fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("/run/secrets/tls/tls.key"),
+  cert: fs.readFileSync("/run/secrets/tls/tls.crt")
+};
+
+const express = require('express')
+
+const app = express();
+
+// parse application/json
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.post('/mutate', (req,res) => {
+    console.log(JSON.stringify(req.body));
+    res.json({ uid: req.body.request.uid, allowed: true });
+});
+
+// app.use((req, res) => {
+//   res.writeHead(200);
+//   res.end("hello world\n");
+// });
+
+app.listen(8000);
+
+https.createServer(options, app).listen(8443);
